9dded8ad5a9e84aaaa767737ff99ace01365a720
vinik <vinicius.kirst@gmail.com>
2016-03-28 02:22:19 -0300

    index test

--- app/Test/Case/Controller/TeamsControllerTest.php
+++ app/Test/Case/Controller/TeamsControllerTest.php
@@ -1,7 +1,7 @@
 <?php
 
 class TeamsControllerTest extends ControllerTestCase {
-    public $fixtures = array('app.team');
+    # public $fixtures = array('app.team');
 
     public function testIndex() {
         $result = $this->testAction('/teams/index');
